457f05c87c03d5c0e58db055ea7d6db085f13f92
Gowtham Ravipati <ravipatigowtham369@gmail.com>
2022-03-21 05:59:06 -0500

    add items fix

--- src/App.js
+++ src/App.js
@@ -2,61 +2,43 @@ import React from 'react';
 import './App.css';
 import { FaTimes } from 'react-icons/fa';
 import JSONDATA from "./mockData.json";
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import Hello from './Hello';
 
 
 function App() {
   const [ searchTerm, setSearchTerm] = useState("");
    
-  const [searchAdd , setsearchAdd]  = useState([
-    {
-      recipe : '',
-      ingrdients : '' 
-    },
-  ]);
+  const [searchAdd , setsearchAdd]  = useState([]);
    
-   const addSearch = () => {
-    <div className="card">
-    <text>{searchAdd.ingrdients}</text>   
-    </div>
-   }
-  const  onAdd = (searchTerm) => {
-     const ingrdients  = ' '
-     const newAddingrdients = searchTerm
-     setsearchAdd([...searchAdd,newAddingrdients])
-     console.log(searchAdd)
+  const  onAdd = (addTerm) => {
+     setsearchAdd([...searchAdd,addTerm])
   }
-
-const onSubmit = (e) => {
-   e.preventDefault()
-   onAdd(searchTerm) 
-   setSearchTerm('')
-}
-
+  useEffect(() => {
+    console.log(searchAdd)
+    }, [searchAdd]);
 
 
   return (
     <div >
         <img src="bgImageMain.jpg" className="bgImage"></img>
-         
         <div className="inputDivStyle">
-             <form className='add-form' onSubmit={onSubmit}>
+             <div className='add-form'>
                   <div className='form-control'>
-                   
-                   <input  type="text" placeholder="Search Recipe" className="InputBoxStyle" value={searchTerm} onInput = {(event) => {setSearchTerm( event.target.value); }}></input>
+                   <input  type="text" placeholder="Search Recipe" className="InputBoxStyle" onChange={(event) => {setSearchTerm( event.target.value)}} ></input>
                    </div>
                      <br></br>
-                    <button type="submit" className="Button">Add</button>
+                    <button type="submit" className="Button" onClick={()=>onAdd(searchTerm)}>Add</button>
                     <>  </>
                     <button title="Search" type="Submit" className="Button">Search</button>
-             </form>
+             </div>
                 <div>
                 
                 {searchAdd.map((val,key)=>{
                        return(
                         <div key={key} >
                         <div className="Addcard">
-                        <text>{val.ingrdients}<FaTimes></FaTimes></text>   
+                        <text>{val}<FaTimes></FaTimes></text>   
                         </div>
                         </div>
                        )
---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hello() {
+  return (
+    <div style={{color:'red'}}>Hello
+    {console.log('Hello')}
+    </div>
+    
+  )
+}
